20af61909856a7f31987e143e4f5d1cb4f36f322
Crosscutt <crosscutt100@gmail.com>
2019-04-28 01:35:40 -0400

    first commit

---
+++ src/Http/Controllers/PerfilController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace wignerll\container\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class PerfilController extends Controller
+{
+    //
+}
---
+++ src/Models/Grupo_Permiso.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace wignerll\container;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Grupo_Permiso extends Model
+{
+    //
+}
